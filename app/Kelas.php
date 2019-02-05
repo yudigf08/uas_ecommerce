dcf945b5ba4ce96e2ff71c75629180279cfef0c7
yudigf <yudigf08@gmail.com>
2019-02-06 02:21:59 +0700

    versi 1.0

---
+++ app/Kelas.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class kelas extends Model
+{
+	public $primaryKey = 'id_kelas';
+
+	protected $table = 't_kelas';
+
+	protected $fillable = [
+		'nama_kelas', 'jurusan'
+	];    
+}
